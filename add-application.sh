6ee28e7a0c61960537567504e931738069873388
gauravjisri <gaurav2125cse@gmail.com>
2024-07-24 17:39:14 +0530

    modified script to promt choices

--- add-application.sh
+++ add-application.sh
@@ -68,34 +68,86 @@ update_overlay_kustomization() {
   done
 }
 
-# Prompt the user for inputs
-prompt_input "Enter the APPLICATION name: " validate_application_name APPLICATION
-check_existing_application "$APPLICATION"
-prompt_input "Enter the TARGET_PORT: " validate_number TARGET_PORT
-read -p "Enter the IMAGE_URL: " IMAGE_URL
-prompt_input "Enter the number of REPLICAS: " validate_number REPLICAS
+# Function to create a new application
+create_application() {
+  while true; do
+    # Prompt the user for inputs
+    prompt_input "Please enter your application name: " validate_application_name APPLICATION
+    check_existing_application "$APPLICATION"
+    prompt_input "Enter the TARGET_PORT: " validate_number TARGET_PORT
+    read -p "Enter the IMAGE_URL: " IMAGE_URL
+    prompt_input "Enter the number of REPLICAS: " validate_number REPLICAS
 
-# Create the application folder
-mkdir -p kustomize/base/${APPLICATION}
+    # Create the application folder
+    mkdir -p kustomize/base/${APPLICATION}
 
-# Replace placeholders in the manifest template and create the Kubernetes manifest
-sed -e "s|{{APPLICATION}}|${APPLICATION}|g" \
-    -e "s|{{TARGET_PORT}}|${TARGET_PORT}|g" \
-    -e "s|{{IMAGE_URL}}|${IMAGE_URL}|g" \
-    -e "s|{{REPLICAS}}|${REPLICAS}|g" \
-    kustomize/template/application.yaml > kustomize/base/${APPLICATION}/${APPLICATION}.yaml
+    # Replace placeholders in the manifest template and create the Kubernetes manifest
+    sed -e "s|{{APPLICATION}}|${APPLICATION}|g" \
+        -e "s|{{TARGET_PORT}}|${TARGET_PORT}|g" \
+        -e "s|{{IMAGE_URL}}|${IMAGE_URL}|g" \
+        -e "s|{{REPLICAS}}|${REPLICAS}|g" \
+        kustomize/template/application.yaml > kustomize/base/${APPLICATION}/${APPLICATION}.yaml
 
-# Create the kustomization.yaml file
-cat <<EOF > kustomize/base/${APPLICATION}/kustomization.yaml
+    # Create the kustomization.yaml file
+    cat <<EOF > kustomize/base/${APPLICATION}/kustomization.yaml
 resources:
   - ${APPLICATION}.yaml
 EOF
 
-echo "Kubernetes manifest file created at kustomize/base/${APPLICATION}/${APPLICATION}.yaml"
-echo "kustomization.yaml file created at kustomize/base/${APPLICATION}/kustomization.yaml"
+    echo "Kubernetes manifest file created at kustomize/base/${APPLICATION}/${APPLICATION}.yaml"
+    echo "kustomization.yaml file created at kustomize/base/${APPLICATION}/kustomization.yaml"
 
-# Prompt the user for environment variables
-read -p "Enter the environment variables (separated by space): " ENV_VARS
+    read -p "Do you want to add another application? (yes/no): " continue_choice
+    if [[ "$continue_choice" != "yes" ]]; then
+      echo "Aborting."
+      break
+    fi
+  done
+}
 
-# Update the overlay kustomization.yaml files
-update_overlay_kustomization "$APPLICATION" "$ENV_VARS"
+# Function to add an existing application to specific environments
+add_existing_application() {
+  while true; do
+    prompt_input "Please enter your application name: " validate_application_name APPLICATION
+
+    # Check if the application YAML file exists
+    if [[ ! -f "kustomize/base/${APPLICATION}/${APPLICATION}.yaml" ]]; then
+      echo "The application ${APPLICATION} does not exist. Aborting."
+      exit 1
+    fi
+
+    while true; do
+      read -p "Please enter the environment names (separated by space) in which you want to add the application: " ENVIRONMENTS
+
+      # Update the overlay kustomization.yaml files
+      update_overlay_kustomization "$APPLICATION" "$ENVIRONMENTS"
+
+      read -p "Do you want to add the application to other environments? (yes/no): " continue_env
+      if [[ "$continue_env" != "yes" ]]; then
+        break
+      fi
+    done
+
+    read -p "Do you want to add another application? (yes/no): " continue_choice
+    if [[ "$continue_choice" != "yes" ]]; then
+      echo "Aborting."
+      break
+    fi
+  done
+}
+
+# Main menu
+while true; do
+  echo "Please select the action you want to do:"
+  echo "1. Create an application"
+  echo "2. Add an existing application"
+  echo "3. Abort"
+  read -p "Enter your choice (1, 2, or 3): " choice
+
+  case "$choice" in
+    1) create_application;;
+    2) add_existing_application;;
+    3) echo "Aborting."; exit 0;;
+    *) echo "Invalid choice. Please enter 1, 2, or 3.";;
+  esac
+done
